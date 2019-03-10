41195b048d2973bd9fcfdf487f0b17080573cbc0
lens <lens.huygh@gmail.com>
2019-03-10 01:42:37 +0100

    ch2 pt.1 fin

--- src/main/java/com/lens/learningspringboot/learningspringbootch2/LearningSpringBootCh2Application.java
+++ src/main/java/com/lens/learningspringboot/learningspringbootch2/LearningSpringBootCh2Application.java
@@ -2,15 +2,21 @@ package com.lens.learningspringboot.learningspringbootch2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class LearningSpringBootCh2Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBootCh2Application.class, args);
+	}
 
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 
 		/*Flux.just("alpha","bravo","charlie")
 				.map(String::toUpperCase)
@@ -18,18 +24,24 @@ public class LearningSpringBootCh2Application {
 				.groupBy(String::toString)
 				.sort((o1,o2) -> o1.key().compareTo(o2.key()))
 				.flatMap(group -> Mono.just(group.key()).and(group.count()))
-				.map(keyAndCount ->
-						keyAndCount.getT1() + " => " + keyAndCount.getT2())
+				.map(keyAndCount -> keyAndCount.getT1() + " => " + keyAndCount.getT2())
 				.subscribe(System.out::println);*/
-
-		Flux.just("alpha","bravo","charlie")
+/*
+		Flux.just("-----------------------------")
+				.subscribe(System.out::println);
+*/
+		/*Flux.just("alpha","bravo","charlie")
 				.map(String::toUpperCase)
 				.flatMap(s -> Flux.fromArray(s.split("")))
 				.groupBy(String::toString)
+				.sort(Comparator.comparing(GroupedFlux::key))
 				.flatMap(group -> Mono.just(group.key()).and(group.count()))
-				//.map(keyAndCount -> keyAndCount.toString())
-				.subscribe(System.out::println);
-	}
+				.map(keyAndCount ->	keyAndCount.getT1() + " => " + keyAndCount.getT2())
+				.subscribe(System.out::println);*/
+
+
+
+
 
 }
 
